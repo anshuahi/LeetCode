5143b745585f4be1475c3310d6bfb74af5050e4d
Anshu Ahirwar <anshuahi@Anshus-MacBook-Air.local>
2024-03-21 10:53:09 +0530

    added cheetSheet

---
+++ CheetCode/cheatSheet.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include "linkedList.h"
+#include "tree.h"
+
+using namespace std;
+
+#define pb push_back
+#define pii pair<int, int>
+
+
+    // check if a char is vowel
+    bool isVowel(char c){
+        char c1 = tolower(c);
+        return c1 == 'a' || c1 == 'e' || c1 == 'i' || c1 == 'o' || c1 == 'u';
+    }
+
+    // binarySearch
+    bool binarySearch(vector<int> &arr, int target){
+        int st = 0, en = arr.size()-1;
+
+        while(st <= en){
+            int mid = (st+en)/2;
+            if(target == arr[mid]){
+                return true;
+            }
+            else if(target < mid){
+                en = mid-1;
+            }
+            else {
+                st = mid+1;
+            }
+        }
+        return false;
+    }
+
+    // overlap using vector<int>
+    bool isOverlap(vector<int> &a, vector<int> &b){
+        if(a[0] <= b[0] && b[0] <= a[1]){
+            return true;
+        }
+        if(b[0] <= a[0] && a[0] <= b[1]){
+            return true;
+        }
+        return false;
+    }
+
+    // overlap using pii<int, int>
+    bool isOverlap(pii &a, pii &b){
+        if(a.first <= b.first && b.first <= a.second){
+            return true;
+        }
+        if(b.first <= a.first && a.first <= b.second){
+            return true;
+        }
+        return false;
+    }
+
+    // reverse a linked-list
+    ListNode* reverseList(ListNode* head) {
+        if(!head)
+            return head;
+        ListNode* p = NULL;
+
+        while(head){
+            p = new ListNode(head->val, p);
+            head = head->next;
+        }
+        return p;
+    }
+
+int main() {
+    return 0;
+}
---
+++ CheetCode/linkedList.h
@@ -0,0 +1,10 @@
+#include <iostream>
+// Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
