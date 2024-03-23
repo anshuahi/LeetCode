0c3ea35794086ab8565d2e5114d59d79d71f9994
Anshu Ahirwar <anshuahi@Anshus-MacBook-Air.local>
2024-03-23 08:41:01 +0530

    23-march 2024: Daily challenge

---
+++ daily-challenges-2024/23_03_2024.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+   
+    void reorderList(ListNode* head) {
+        if(!head || !head->next || !head->next->next)
+            return;
+        // Find the middle of the list
+        ListNode* slow = head, *fast = head;
+        while(fast->next && fast->next->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // Reverse the second half of the list
+        ListNode* prev = NULL, *curr = slow->next, *nextNode;
+        while(curr != NULL){
+            nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        slow->next = NULL;
+
+        // merge the 2 halves
+        ListNode* first = head, *second = prev;
+        while(first && second){
+            ListNode* first_next = first->next;
+            ListNode* second_next = second->next;
+
+            first->next = second;
+            second->next = first_next;
+
+            first = first_next;
+            second = second_next;
+        }
+    }
+};
