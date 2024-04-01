a6e2278d7c2273be44a59b25845cb0fa3a9dab57
Anshu Ahirwar <anshuahi@Anshus-MacBook-Air.local>
2024-04-02 02:09:14 +0530

    1-April 2024: Daily challenge

---
+++ daily-challenges-2024/01_04_2024.cpp
@@ -0,0 +1,31 @@
+/*
+
+LeetCode-POTD: 58. Length of Last Word
+
+POTD Link - https://leetcode.com/problems/length-of-last-word/description/
+
+Approach :     
+    1. remove the whitespace at the end
+    2. count the consecutive chars from the end.
+
+*/
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        while(s.size() > 0 && s.back() == ' '){
+            s.pop_back();
+        }
+        int i = s.size()-1;
+        int ans  = 0;
+        while(i >= 0){
+            if(s[i] == ' '){
+                break;
+            }
+            ans++;
+            i--;
+        }
+        return ans;
+    }
+};
+
